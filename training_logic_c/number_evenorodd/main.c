c556d0306d6fed4d86483a7ea616744414d6ef2d
Mario Nelson <marionelson616@gmail.com>
2024-05-29 21:55:06 -0400

    Adding the second nano project, a program that checks if a number is even or odd

---
+++ training_logic_c/number_evenorodd/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int num;
+    
+    printf("Enter the number: \n");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("The number is even.\n");
+    }
+    else {
+        printf("The number is odd.\n");
+    }
+    printf("Chosen number: %d\n", num);
+    return 0;
+}
