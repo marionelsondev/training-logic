33bae7c1acf899816bb2dee828299dac1069106e
Mario Nelson <marionelson616@gmail.com>
2024-06-03 19:27:04 -0400

    Updating nano project #2, loops were added: one to make the system print numbers until it reaches the number entered by the user, and another loop to indicate whether each number is odd or even.

--- training_logic_c/number_evenorodd/main.c
+++ training_logic_c/number_evenorodd/main.c
@@ -3,15 +3,20 @@
 int main () {
     int num;
     
-    printf("Enter the number: \n");
-    scanf("%d", &num);
+    do {
+        printf("Enter a positive number: \n");
+        scanf("%d", &num);
+        if (num <= 0) {
+            printf("Invalid number, you entered a value less than or equal to zero, please try again.\n");
+        }
+    } while (num <= 0);
 
-    if (num % 2 == 0) {
-        printf("The number is even.\n");
+    for (int i = 1; i <= num; i++) {
+        if (i % 2 == 0) {
+            printf("The number '%d' is even.\n", i);
+        } else {
+            printf("The number '%d' is odd.\n", i);
+        }   
     }
-    else {
-        printf("The number is odd.\n");
-    }
-    printf("Chosen number: %d\n", num);
     return 0;
 }
