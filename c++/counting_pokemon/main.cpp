133c3a7bb44a086ea5faa88f5e1135703b4448d1
Mário Nelson <gbdev03@gbringel.com>
2024-12-05 17:06:01 -0400

    Adding exercise counting_pokemon

---
+++ c++/counting_pokemon/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k, j, h, nk, nj, nh;
+
+    cin >> k >> j >> h >> nk >> nj >> nh;
+
+    cout << k + nk << ' ' << j + nj << ' ' << h + nh << ' ' << endl;
+
+    return 0;
+}
