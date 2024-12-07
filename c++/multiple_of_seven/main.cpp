ded5038316749d0a3e62ef4913565d95eff92b5b
Mário Nelson <gbdev03@gbringel.com>
2024-12-07 17:19:10 -0400

    Adding exercise multiple_of_seven

---
+++ c++/multiple_of_seven/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+    
+    if (n % 7 == 0) {
+        cout << "É MULTIPLO DE 7" << endl;
+    } else {
+        cout << "ESSE NÃO É" << endl;
+    }
+
+    return 0;
+}
